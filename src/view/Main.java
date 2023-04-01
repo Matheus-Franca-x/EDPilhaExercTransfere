5734094fab09d05e05fea22a65e007cce8fad2c3
Matheus <matheussss6613@gmail.com>
2023-03-31 22:21:53 -0300

    test

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,16 @@
 package view;
 
+import libPilhaInt.PilhaInt;
 import libPilhaInt.PilhaString;
 
 public class Main 
 {
 	
-	PilhaString pilhaStr = new PilhaString();	
+	PilhaString pilhaStr = new PilhaString();
+	PilhaInt pilhaInt = new PilhaInt();
+	
+	
+	
+	
 	
 }
