fa4003f23bfb0e16de3fdbcc961c1cd2d7a8a9a3
matheus <matheussss6613@gmail.com>
2023-04-11 23:53:31 -0300

    decToBin concluido

--- src/controller/ConverteController.java
+++ src/controller/ConverteController.java
@@ -6,23 +6,33 @@ import libPilhaInt.PilhaString;
 
 public class ConverteController 
 {
-	private PilhaInt pInt = new PilhaInt();
-	private PilhaString pStr = new PilhaString();
+//	public PilhaString pStr = new PilhaString();
 	
 	public ConverteController()
 	{
 		super();
 	}
 	
-	public void decToBin(int valor)
+	public void decToBin(int valor) 
 	{
+		PilhaInt pInt = new PilhaInt();
 		int bin = 0;
-		while(valor <= 0)
+		String met = "";
+		while(valor > 0)
 		{
 			bin = valor % 2;
 			pInt.push(bin);
 			valor /= 2;
 		}
+		while(!pInt.isEmpty())
+		{
+			try {
+				met = met + pInt.pop();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(met);
 	}
 
 }
