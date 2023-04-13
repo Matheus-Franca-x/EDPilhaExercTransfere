3db076fe9ed62b4429005431e61bf368539632ad
matheus <matheussss6613@gmail.com>
2023-04-13 00:04:03 -0300

    Fim

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,18 +1,16 @@
 package view;
 
 import controller.ConverteController;
-import controller.FatorialController;
 
 public class Main 
 {
 	
 	public static void main(String[] args)
 	{
-//		ConverteController pilhaBin = new ConverteController();
-//		pilhaBin.decToBin(8);
+		ConverteController pilhaBin = new ConverteController();
+		pilhaBin.decToBin(8);
 		
-		FatorialController pilhaFat = new FatorialController();
-		pilhaFat.fat(0);
+
 		
 		
 		
